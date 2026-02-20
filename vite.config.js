5d4637edfb1e4560fa41bf9a8116c28d750c271f
Nafiann <fadhliarekmalang@gmail.com>
2026-02-20 12:29:16 +0700

    fix peta mobile

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite"; // Plugin wajib buat Tailwind v4
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
-    tailwindcss(), // Aktifkan mesin Tailwind-nya di sini
+    tailwindcss(),
   ],
   build: {
     rollupOptions: {
       input: {
-        main: "index.html", // Halaman Utama
-        peta: "peta.html", // Halaman Peta Candi
-      },
-    },
-  },
+        main: 'index.html',
+        peta: 'peta.html',
+      }
+    }
+  }
 });
